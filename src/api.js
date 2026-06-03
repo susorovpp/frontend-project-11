6b99423e9c1f43c6b736492b381a87386e8cd6f5
Pavel Susorov <pavelsusorov@Pavels-MacBook-Pro-2.local>
2026-06-03 18:16:11 +0300

    for debugging

--- src/api.js
+++ src/api.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const getProxyUrl = (url) => {
   const encodedUrl = encodeURIComponent(url);
 
-  return `https://allorigins.hexlet.app/raw?disableCache=true&url=${encodedUrl}`;
+  return `https://allorigins.hexlet.app/get?disableCache=true&url=${encodedUrl}`;
 };
 
 export const getFeed = (url) => {
   const proxyUrl = getProxyUrl(url);
 
-  return axios.get(proxyUrl).then((response) => response.data);
+  return axios.get(proxyUrl).then((response) => response.data.contents);
 };
